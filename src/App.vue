b025502f0de63c4078870e9790dcc562bc43c857
gokaju <gokaju23@gmail.com>
2019-11-05 17:58:01 -0500

    --

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-app >
+
     <componet :is="layout" >
       <router-view  />
     </componet>
-  </v-app>
+
 </template>
 
 <script>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// layout importation 
+import Default from "./layouts/Default.vue"
+import Home from "./layouts/Home.vue"
+Vue.component("default-layout",Default)
+Vue.component("home-layout",Home)
+
 new Vue({
   router,
   store,
