4d1e472b112da51fdf6d673939f50777de39c7f5
Gokaju <gokaju@iMac-de-Gokaju.local>
2019-12-12 20:59:21 -0500

    --

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,25 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { expect } from "chai";
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+import DFriendCard from "@/components/DFriendCard.vue";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
-  })
-})
+    });
+    expect(wrapper.text()).to.include(msg);
+  });
+});
+describe("DFriendCard.vue", () => {
+  it("renders props.msg when passed", () => {
+    const username = "username";
+    const name = "name";
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { username, name }
+    });
+    expect(wrapper.text()).to.include(username);
+    expect(wrapper.text()).to.include(name);
+  });
+});
